53974b4b5ce5d1ca03d51760ad13ba3d4fc482f8
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-05-06 20:32:56 +0200

    Very first Components

---
+++ src/components/FirstComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function FirstComponent() {
+    return (
+        <div>
+            <h1>Hello again</h1>
+        </div>
+    );
+}
+
+export default FirstComponent;
