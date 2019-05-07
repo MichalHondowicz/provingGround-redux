78cd0f1af5ea7ecf94a93808421ed0d4d39bd39f
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-05-07 19:59:33 +0200

    Event listener implemented

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 
 class Avatar extends Component {
+
+    onClickHandler() {
+        alert("Just clicked!")
+    }
+
     render() {
         const {login = ""} = this.props;
         const imgUrl = `https://api.adorable.io/avatars/55/${login}.png`;
         return (
-            <img src={imgUrl} className="ui mini rounded image"/>
+            <button onClick={this.onClickHandler}>
+                <img src={imgUrl} className="ui mini rounded image"/>
+            </button>
         )
     }
 }
