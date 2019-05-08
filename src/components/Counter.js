fc8a2648ccaee1419046e608f9907f1dd0e6b60f
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-05-08 19:25:01 +0200

    Multiple setState introduced

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,28 +5,37 @@ class Counter extends Component {
 
     constructor() {
         super();
-        this.state = {score: 0}
+        this.state = {
+            score: 0,
+            clickCount: 0
+        }
     }
 
     plusButtonHandler() {
-        this.setState({
-            score: this.state.score + 1
+        this.setState(prevState => {
+            return {
+                score: prevState.score + 1,
+                clickCount: prevState.clickCount + 1
+            }
         })
     }
 
     minusButtonHandler() {
-        this.setState({
-            score: this.state.score - 1
+        this.setState(prevState => {
+            return {
+                score: prevState.score - 1,
+                clickCount: prevState.clickCount + 1
+            }
         })
     }
 
     render() {
-
         return (
             <div>
                 <output>{this.state.score}</output>
                 <button onClick={this.plusButtonHandler.bind(this)}>+</button>
                 <button onClick={this.minusButtonHandler.bind(this)}>-</button>
+                <output>{this.state.clickCount}</output>
             </div>
         )
     }
