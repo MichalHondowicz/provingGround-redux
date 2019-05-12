456bf9c8261e3ddb97483186fba6d3bb74a259c4
MichalHondowicz <40538831+MichalHondowicz@users.noreply.github.com>
2019-05-12 12:43:20 +0200

    componentWillReceiveProps implemented

--- src/Store.js
+++ src/Store.js
@@ -1,6 +1,7 @@
 import {applyMiddleware, createStore, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers'
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const initialState = {};
 const middleWare = [thunk];
